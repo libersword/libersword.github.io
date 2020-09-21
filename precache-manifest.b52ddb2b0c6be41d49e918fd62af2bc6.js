cbcbd52920f2914512ae3dbe73ac832aa12f441f
Lindsey Brown <libersword@gmail.com>
2020-09-20 18:40:18 -0600

    Updates

---
+++ precache-manifest.b52ddb2b0c6be41d49e918fd62af2bc6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50baa97c2ce6deab10bd35f07dc1c8b7",
+    "url": "/lindseys.work/index.html"
+  },
+  {
+    "revision": "dba647f94a138c786075",
+    "url": "/lindseys.work/static/css/2.257f4af1.chunk.css"
+  },
+  {
+    "revision": "d3218e412dbbb779e52b",
+    "url": "/lindseys.work/static/css/main.a1b0e1ca.chunk.css"
+  },
+  {
+    "revision": "dba647f94a138c786075",
+    "url": "/lindseys.work/static/js/2.1a016312.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/lindseys.work/static/js/2.1a016312.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3218e412dbbb779e52b",
+    "url": "/lindseys.work/static/js/main.61902fbe.chunk.js"
+  },
+  {
+    "revision": "a8e360d91230767b2d8b",
+    "url": "/lindseys.work/static/js/runtime-main.5ce7c1dd.js"
+  }
+]);
